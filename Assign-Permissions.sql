6f26510bda301977719e88c8e4993e95638d80a5
Maryam Kaiser <maryamkaiser@hotmail.com>
2016-04-20 14:09:50 +0500

    Assign permissions to authenticated users

---
+++ Assign-Permissions.sql
@@ -0,0 +1,10 @@
+---============ Assigning Administrator Roles=================================
+
+	Insert Into BusinessEntityRolesMapping (BusinessEntityId, RoleId, PermissionId) Values    (@BE_Sch_TEMP, @Admin_Role_Id, 1)
+	Insert Into BusinessEntityRolesMapping (BusinessEntityId, RoleId, PermissionId) Values    (@BE_Sch_TEMP, @Admin_Role_Id, 2)
+	Insert Into BusinessEntityRolesMapping (BusinessEntityId, RoleId, PermissionId) Values    (@BE_Sch_TEMP, @Admin_Role_Id, 3)
+	Insert Into BusinessEntityRolesMapping (BusinessEntityId, RoleId, PermissionId) Values    (@BE_Sch_TEMP, @Admin_Role_Id, 4)
+	Insert Into BusinessEntityRolesMapping (BusinessEntityId, RoleId, PermissionId) Values    (@BE_Sch_TEMP, @Admin_Role_Id, 5)
+	Insert Into BusinessEntityRolesMapping (BusinessEntityId, RoleId, PermissionId) Values    (@BE_Sch_TEMP, @Admin_Role_Id, 7)
+	Insert Into BusinessEntityRolesMapping (BusinessEntityId, RoleId, PermissionId) Values    (@BE_Sch_TEMP, @Admin_Role_Id, 8)
+	
